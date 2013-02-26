6d65567706564f34f006eb8ab35247ebed9e94b3
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-02-26 17:11:51 -0500

    Update midi section

--- src/tutorial/midi.clj
+++ src/tutorial/midi.clj
@@ -2,27 +2,33 @@
   (:import (javax.sound.midi MidiSystem Synthesizer)))
 
 (def notes
-  [{:note 60 :duration 1}
-   {:note 62 :duration 1}
-   {:note 64 :duration 1}
-   {:note 60 :duration 1}
-   {:note 60 :duration 1}
-   {:note 62 :duration 1}
-   {:note 64 :duration 1}
-   {:note 60 :duration 1}
-   {:note 64 :duration 1}
-   {:note 65 :duration 1}
-   {:note 67 :duration 2}
-   {:note 64 :duration 1}
-   {:note 65 :duration 1}
-   {:note 67 :duration 2}])
+     [{:note 52 :duration 3}
+      {:note 52 :duration 1}
+      {:note 55 :duration 1}
+      {:note 52 :duration 1}
+      {:note 50 :duration 1}
+      {:note 48 :duration 2}
+      {:note 47 :duration 2}
+      {:note 52 :duration 3}
+      {:note 52 :duration 1}
+      {:note 55 :duration 1}
+      {:note 52 :duration 1}
+      {:note 50 :duration 1}
+      {:note 48 :duration 1}
+      {:note 50 :duration 1}
+      {:note 48 :duration 1}
+      {:note 47 :duration 2}])
 
-(defn play-a-note [note velocity duration]
-  (with-open [synth (doto (MidiSystem/getSynthesizer) .open)]
-    (let [channel (aget (.getChannels synth) 0)]
+(defn play-a-note 
+  ([note velocity duration]
+   (play-a-note note velocity duration 0))
+  ([note velocity duration instrument] 
+   (with-open [synth (doto (MidiSystem/getSynthesizer) .open)]
+    (let [channel (aget (.getChannels synth) instrument)]
       (.noteOn channel note velocity)
       (Thread/sleep duration)
-      (.noteOff channel note))))
+      (.noteOff channel note)))))
+
+(count (.getChannels (MidiSystem/getSynthesizer)))
 
 (play-a-note 60 127 3000)
-
